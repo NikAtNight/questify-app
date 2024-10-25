22680d3b52ec73d3b8d68c893f1ceb4667bb739d
Nikhil <98060511+NikAtNight@users.noreply.github.com>
2024-10-24 23:22:57 -0400

    fix page layout

--- app/(protected)/home/archives.tsx
+++ app/(protected)/home/archives.tsx
@@ -1,12 +1,23 @@
+import { useRouter } from "expo-router";
+import { ChevronLeft, Dot } from "lucide-react-native";
 import { View } from "react-native";
 
 import { SafeAreaView } from "@/components/safe-area-view";
+import { Button } from "@/components/ui/button";
 import { H2 } from "@/components/ui/typography";
-
 export default function AbandonedQuests() {
+	const router = useRouter();
 	return (
-		<SafeAreaView className="flex flex-1 bg-background">
-			<H2 className="text-center">Archived Quests</H2>
+		<SafeAreaView className="flex flex-1 bg-background p-4">
+			<View className="flex flex-row items-center justify-between w-full">
+				<Button onPress={() => router.back()} variant="ghost" size="icon">
+					<ChevronLeft />
+				</Button>
+				<H2>Archived Quests</H2>
+				<Button variant="ghost" size="none" className="invisible">
+					<Dot />
+				</Button>
+			</View>
 		</SafeAreaView>
 	);
 }
